13e58689287799d6394297949505cf3cd579beba
Hasbib <shohiburrotibhabib@gmail.com>
2024-10-31 13:04:51 +0700

    push

---
+++ src/pages/role/superadmin/cms/Sponsor.vue
@@ -0,0 +1,42 @@
+<script setup>
+import NavbarSA from '@/layout/NavbarSA.vue';
+import SidebarSA from '@/layout/SidebarSA.vue';
+
+import { ref, onUnmounted, onMounted } from 'vue';
+
+const isSidebarVisible = ref(true);
+
+const checkWindowSize = () => {
+    isSidebarVisible.value = window.innerWidth >= 770;
+};
+
+onMounted(() => {
+    checkWindowSize();
+    window.addEventListener('resize', checkWindowSize);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('resize', checkWindowSize);
+})
+
+</script>
+<template>
+    <div class="navbg-sa">
+        <!-- NAVBAR START -->
+        <NavbarSA />
+        <!-- NAVBAR END -->
+
+        <!-- SIDEBAR START -->
+        <SidebarSA v-if="isSidebarVisible" />
+        <!-- SIDEBAR END -->
+
+
+        <div id="contentsa" class="dashboard-sa">
+            <div class="container mt-80">
+                <div class="row">
+                    <h1> Hlaman Sponsor</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,79 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+//Teacher
+import DashboardTC from '@/pages/role/teacher/Dashboard.vue';
+
+//Superadmin
+import DashboardSA from '@/pages/role/superadmin/Dashboard.vue';
+import CourseManajemen from '@/pages/role/superadmin/CourseManajemen.vue';
+import FaqAdmin from '@/pages/role/superadmin/Faq.vue';
+import HelpCenter from '@/pages/role/superadmin/HelpCenter.vue';
+import Sponsor from '@/pages/role/superadmin/cms/Sponsor.vue';
+import MediaPartner from '@/pages/role/superadmin/cms/MediaPartner.vue';
+import SosialMedia from '@/pages/role/superadmin/cms/SosialMedia.vue';
+import AboutUs from '@/pages/role/superadmin/cms/AboutUs.vue';
+import MasterDataCategory from '@/pages/role/superadmin/masterdata/MasterDataCategory.vue';
+import MasterDataSkill from '@/pages/role/superadmin/masterdata/MasterDataSkill.vue';
+import MasterDataLevelCourse from '@/pages/role/superadmin/masterdata/MasterDataLevelCourse.vue';
+import MasterDataLevelTeacher from '@/pages/role/superadmin/masterdata/MasterDataLevelTeacher.vue';
+import MasterDataTools from '@/pages/role/superadmin/masterdata/MasterDataTools.vue';
+
+//Auth Teacher
+import RegistrasiTeacher from '@/pages/auth/Registrasi-teacher.vue';
+import LoginTeacher from '@/pages/auth/Login-teacher.vue';
+
+import LoginAdmin from '@/pages/auth/Login-admin.vue';
+
+import Error from '@/pages/404/404.vue';
+
+//TEST
+import Test from '@/pages/Test.vue';
+
+//Sementara Auth Teacher
+import MasterData1 from '@/pages/role/superadmin/MasterData1.vue';
+import MasterData2 from '@/pages/role/superadmin/MasterData2.vue';
+import MasterData3 from '@/pages/role/superadmin/MasterData3.vue';
+
+const routes = [
+
+    //Auth Superadmin
+  { path: '/', component: LoginAdmin },
+
+  //Teacher
+  { path: '/dashboard-teacher', component: DashboardTC, meta: { requiresAuth: true, role: 'teacher' } },
+
+  //Superadmin
+  { path: '/dashboard-superadmin', component: DashboardSA },
+  { path: '/course-manajemen', component: CourseManajemen },
+  { path: '/faq-admin', component: FaqAdmin },
+  { path: '/help-center', component: HelpCenter },
+  { path: '/cms/media-partner', component: MediaPartner },
+  { path: '/cms/sosial-media', component: SosialMedia },
+  { path: '/cms/sponsor', component: Sponsor },
+  { path: '/cms/about-us', component: AboutUs },
+  { path: '/master-data/category', component: MasterDataCategory },
+  { path: '/master-data/skills', component: MasterDataSkill },
+  { path: '/master-data/level-course', component: MasterDataLevelCourse },
+  { path: '/master-data/level-teacher', component: MasterDataLevelTeacher },
+  { path: '/master-data/tools', component: MasterDataTools },
+
+  { path: '/registrasi-teacher', component: RegistrasiTeacher },
+  { path: '/login-teacher', component: LoginTeacher },
+
+  { path: '/404', component: Error },
+
+  //TEST
+  { path: '/test', component: Test },
+
+  //Sementara Auth Teacher
+  { path: '/master-data-1', component: MasterData1 },
+  { path: '/master-data-2', component: MasterData2 },
+  { path: '/master-data-3', component: MasterData3 },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
